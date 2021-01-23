8eb57e03854ffcbf41512115b943f97a486d1b7a
enekaas <rezaenekaas@gmail.com>
2021-01-23 04:05:02 +0330

    update code

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const ul = document.querySelector('.list-group')
 let input = document.querySelector('input')
+const ln = document.querySelector('.last-note')
 const cardHeader = document.querySelector('.card-header')
 const cardText = document.querySelector('.card-text')
 let date = new Date().toLocaleString()
@@ -11,4 +12,10 @@ function addNote(){
   ul.innerHTML += `<li class="list-group-item">${input.value}</li>`
   input.value = ''
 }
-ul.innerHTML += `<li class="list-group-item">${localStorage.getItem('notes')}</li>`;
+
+if (localStorage.getItem('notes') !== null){
+  ln.innerText = 'your last note:'
+  ul.innerHTML += `<li class="list-group-item">${localStorage.getItem('notes')}</li>`;
+} else {
+  ln.innterText = "You Haven't any note"
+}
