578cede7028325f87a7d95c3eff7b13fdd96d5b8
Alireza Saeedi <70912682+Enekaas@users.noreply.github.com>
2021-03-20 11:28:01 +0330

    update code

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ const ul = document.querySelector('.list-group')
 let input = document.querySelector('input')
 const cardHeader = document.querySelector('.card-header')
 const cardText = document.querySelector('.card-text')
-let date = new Date().toLocaleString()
-cardText.innerText = Date()
-cardHeader.innerText += ` | ${date}`
+setInterval(() => {
+cardText.innerText = new Date().toLocaleString()
+cardHeader.innerText += ` | ${new Date().toLocaleString()}`
+},1000);
 
 function addNote(){
   localStorage.setItem('notes',input.value)
-  ul.innerHTML += `<li class="list-group-item">${input.value}</li>`
+  ul.innerHTML = `<li class="list-group-item">${input.value}</li>`
   input.value = ''
 }
-ul.innerHTML += `<li class="list-group-item">${localStorage.getItem('notes')}</li>`;
+ul.innerHTML += `<li class="list-group-item">${localStorage.getItem('notes') || 'شما هیچ یادداشتی ندارید'}</li>`;
